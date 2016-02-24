069df935511cf196e3661a0b52efda8c41b4a8ca
Eric Norgren <norgren.eric@gmail.com>
2016-02-24 16:33:51 +0100

    Fixed Euler

--- Assets/Script/RK4.cs
+++ Assets/Script/RK4.cs
@@ -3,18 +3,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class RK4 {
-	public float timestep = 0.05;
+	public float timestep = 0.05f;
 
 	public void Euler(List<Point> points) {
 		int amount = points.Count;
 		Point point;
 		for (int i = 0; i < amount; ++i) {
-			point = points [i];
-			point.CalculateForces ();
-			point.velocity += timestep * point.force / point.mass;
-			point.position += timestep * point.velocity;
-
+			points[i].CalculateForces ();
 
 		}
+		for (int i = 0; i < amount; ++i) {
+			point = points [i];
+			point.velocity += timestep * point.force / point.mass;
+			point.position += timestep * point.velocity;
+		}
+
 	}
 }

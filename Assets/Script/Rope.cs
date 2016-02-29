ac8224d3760a637516bc263a7867a9521d6c4089
Jonas Dahl <jonas@jdahl.se>
2016-02-29 19:12:49 +0100

    Converted line endings to unix and commented

--- Assets/Script/Rope.cs
+++ Assets/Script/Rope.cs
@@ -4,40 +4,58 @@ using System.Collections.Generic;
 using System.Linq;
 
 public class Rope : PointController {
-
+	// Start point for rope
 	private Vector3 startPoint;
+
+	// End point for rope
 	private Vector3 endPoint;
 
+	// Anchor points
 	private List<Point> anchors;
 
-	public void Start() {
-		
-	}
+	/**
+	 * Empty constructor.
+	 */
+	public void Start() { }
 
+	/**
+	 * Inits this rope.
+	 */
 	public void init(Vector3 start, Vector3 end, bool anchor, int amountOfPoints, string name) {
-		points = new List<Point> ();
+		// Inits points list, sets posiiton and direction
+		points = new List<Point>();
 		Vector3 position = start;
 		Vector3 ropeDirection = (end - start) / amountOfPoints;
 
+		// Generates rope
 		for (int i = 0; i < amountOfPoints; ++i) {
-			points.Add(createPoint (position, name + i));
+			points.Add(createPoint(position, name + i));
 			position += ropeDirection;
 		}
+
+		// Adds links between internal points
 		for (int i = 0; i < amountOfPoints - 1; ++i) {
 			points [i].AddNeigbour(points [i + 1]);
 		}
 
+		// Sets anchor if so
 		if (anchor) {
 			anchors = new List<Point> ();
-			anchors.Add (points [0]);
+			anchors.Add(points [0]);
 			anchors.Add(points[amountOfPoints - 1]);
 		}
 	}
 
+	/**
+	 * Returns number of points.
+	 */
 	public int amountOfPoints() {
 		return points.Count;
 	}
 
+	/**
+	 * Returns a specific point.
+	 */
 	public Point getPoint(int index) {
 		return points[index];
 	}

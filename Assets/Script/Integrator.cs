e26cff376955e5dc4099736d3f3c9c419a8eda78
Eric Norgren <norgren.eric@gmail.com>
2016-03-01 11:12:07 +0100

    Integrator should compile, Jonas is happy

--- Assets/Script/Integrator.cs
+++ Assets/Script/Integrator.cs
@@ -40,37 +40,61 @@ public class Integrator {
 
 	private List<IntegrateData> m_calcData = new List<IntegrateData>();
 	private Action <float> forceFunc;
+	private int numPoints, prevNumPoints;
 
 
-
-	public Integrator(List <Point> points, Action<float> updateForcesFunc){
+	public Integrator(List<PointController> pcl, Action<float> updateForcesFunc){
 		m_calcData = new List<IntegrateData> ();
-		initIntegrateData (points.Count);
+
+		numPoints = 0;
+		prevNumPoints = 0;
+		prepIntegrateData (pcl);
+
 		forceFunc = updateForcesFunc;
 
 	}
 
-	public void integrate(){
+	public void integrate(List<PointController> pcl){
+		PointController pc;
+
+		// a
+
+		evaluate(pcl, forceFunc, 0); //TODO
+		for (int i = 0; i < pcl.Count; ++i) {
+			pc = pcl [i];
+			for (int j = 0; j < pc.getPoints().Count; ++i) {
+				m_calcData [i + j].a = m_calcData [i + j].evalResult;
+			}
+		}
+
 
 	}
 
-	public void evaluate(List <Point> points, Action <float> updateForcesFunc, float timestep){
+	private void evaluate(List<PointController> pcl, Action updateForcesFunc, float timestep){
+		foreach (PointController pc in pcl) {
+			foreach (Point point in pc.getPoints ()) {
 
+			}
+		}
 	}
 
-	public void initIntegrateData(int numPoints){
-		
-		int diff = numPoints - m_calcData.Count;
+	private void prepIntegrateData(List<PointController> pcl){
+		numPoints = 0;
+		foreach (PointController pc in pcl) {
+			numPoints += pc.getPoints().Count;
+		}
+		int diff = numPoints - prevNumPoints;
+		prevNumPoints = numPoints;
 
 		//Do we need more? Create more! Never shrink
 		if (diff > 0){
 			for (int i = 0; i < diff; i++){
-				m_calcData.Add(new CalcData());
+				m_calcData.Add(new IntegrateData());
 			}
 		}
 
 		for (int i = 0; i < numPoints; i++){
-			m_calcData[i].evalResult.Reset();
+			m_calcData[i].evalResult.reset();
 		}
 	}
 
